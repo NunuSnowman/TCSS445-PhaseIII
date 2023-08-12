3d9bf6da277f24149d4f44726e9074fc74b1f8a1
alan8r <alan8r@gmail.com>
2023-08-12 15:27:03 -0700

    added server/client communication and tests via index.html

---
+++ getfromserver.js
@@ -0,0 +1,71 @@
+
+let button = document.querySelector('#button')
+const divResponse = document.querySelector('#response')
+
+function serverRequest (method, url) {
+    return new Promise(function (resolve, reject) {
+        var xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+        xhr.onload = function () {
+            if (this.status >= 200 && this.status < 300) {
+                resolve(xhr.response);
+            } else {
+                reject({
+                    status: this.status,
+                    statusText: xhr.statusText
+                });
+            }
+        };
+        xhr.onerror = function () {
+            reject({
+                status: this.status,
+                statusText: xhr.statusText
+            });
+        };
+        xhr.send();
+    });
+  }
+
+
+button1.onclick = () => {
+    serverRequest('GET', '//localhost:3000/test')
+    .then(result=>{
+        displayTest(result)
+    })
+    .catch(err=>{
+        console.error(err.statusText)
+    })
+}
+
+button2.onclick = () => {
+    serverRequest('GET', '//localhost:3000/test2')
+    .then(result=>{
+        displayTest(result)
+    })
+    .catch(err=>{
+        console.error(err.statusText)
+    })
+}
+
+function displayTest(result) {
+    divResponse.innerText = "";
+    resultJSON = JSON.parse(result)
+    console.table(resultJSON)
+    stringProps = ""
+    Object.getOwnPropertyNames(resultJSON[0]).forEach(property=>{
+        stringProps += property+','
+    })
+    stringProps = stringProps.substring(0, stringProps.length-1)
+    divResponse.append('['+stringProps+']')
+    divResponse.append(document.createElement('br'))
+    divResponse.append(document.createElement('br'))
+    resultJSON.forEach(item=>{
+        itemString = ""
+        Object.getOwnPropertyNames(item).forEach(property=>{
+            itemString += item[property] + ','
+        })
+        itemString = itemString.substring(0, itemString.length-1)
+        divResponse.append(itemString)
+        divResponse.append(document.createElement('br'))
+    })
+}
--- server.js
+++ server.js
@@ -5,14 +5,14 @@ const mysql = require('mysql');
 const cors = require('cors');
 const app = express();
 //An Express.js middleware setup that enables CORS for the Express application
-app.use(cors());
+app.use(cors('*'));
 
 const db = mysql.createConnection({
   host: 'localhost',
   port: 3306,
-  user: 'root',
-  password: '00000000',
-  database: 'test1'
+  user: 'test',
+  password: 'pass',
+  database: 'group_project'
 });
 
 db.connect((err) => {
@@ -41,9 +41,40 @@ app.get('/borrowed', (req, res) => {
   db.query('SELECT * FROM borrowed', (error, results) => {
     if (error) throw error;
     res.json(results);
+    
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
+const port = 3000
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
 });
+
+/* ########### ALAN TESTING STUFF BELOW ########## */
+
+app.get('/', (req, res) => {
+  if (error) throw error
+  console.log('sent index.html')
+  res.send('index.html')
+})
+
+app.get('/test', (req, res) => {
+
+  let query = `SELECT book_id, title FROM book ORDER BY book_id`
+
+  db.query(query, (error, results) => {
+    if (error) throw error
+    res.json(results)
+  })
+})
+
+app.get('/test2', (req, res) => {
+
+  let query = `SELECT * FROM user`
+
+  db.query(query, (error, results) => {
+    if (error) throw error
+    res.json(results)
+  })
+})
